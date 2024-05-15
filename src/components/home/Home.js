5685b4d86573472ebd1a439417d43487d500537e
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2024-05-15 10:33:18 +0200

    update main_page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./Home.css";
 
 function Home({ text }) {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="home_top"></div>
+      <div className="home_right"></div>
+      <div className="home_top_about">Informacje ogólne</div>
+      <div className="home_left"></div>
+    </div>
+  );
 }
 
 export default Home;
