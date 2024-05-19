a8997e1d8c75116a9c9b1932336c23566a8ae11e
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2024-05-19 10:34:24 +0200

    wip

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Home.css";
 import LANDSCAPE_1280 from "./LANDSCAPE_1280.jpg";
-function Home({ text }) {
+function Home({ text }) 
   return (
     <div className="home">
-      <div className="home_right"></div>
+      <div className="home_right">
+      <div className="home_right_title">GEOPORTAL</div>
+      <div className="home_right_subtitle">
+            Strona internetowa z interaktywną mapą
+ 
+      </div>
 
       <div className="home_top_about">Informacje ogólne</div>
 
