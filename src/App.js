dc6abb4ee891992179f79b8470b486f30be6f7d9
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2024-05-19 20:28:42 +0200

    wip: create services

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
+import Services from "./components/home/services/Services";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  {
+    path: "/services",
+    element: <Services />,
+  },
+]);
 function App() {
   const imie = "tomasz";
 
   return (
     <div className="App">
-      <Home text={imie} />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 import LANDSCAPE_1280 from "./LANDSCAPE_1280.jpg";
 
 function Home({ text }) {
   return (
     <div className="home">
+      <div className="home_top">
+        <div className="home_top_about">Informacje ogólne</div>
+      </div>
       <div className="home_left">
         <img className="logo_policji" src={LANDSCAPE_1280} alt="logo" />
       </div>
@@ -14,8 +18,9 @@ function Home({ text }) {
           Zarządzanie jednostkami policji i policjantami przypisanymi do danej
           jednostki
         </div>
-        <button className="home_right_button">START</button>
-        <div className="home_top_about">Informacje ogólne</div>
+        <Link to="/services">
+          <button className="home_right_button">START</button>
+        </Link>
       </div>
     </div>
   );
