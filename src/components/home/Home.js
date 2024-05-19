a20069c8f9f5f02a2141ff31c836977744a8e058
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2024-05-19 16:48:50 +0200

    wip

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./Home.css";
 import LANDSCAPE_1280 from "./LANDSCAPE_1280.jpg";
-function Home({ text }) 
+
+function Home({ text }) {
   return (
     <div className="home">
-      <div className="home_right">
-      <div className="home_right_title">GEOPORTAL</div>
-      <div className="home_right_subtitle">
-            Strona internetowa z interaktywną mapą
- 
-      </div>
-
-      <div className="home_top_about">Informacje ogólne</div>
-
       <div className="home_left">
-        <img className="plan" src={LANDSCAPE_1280} alt="logo"></img>
+        <img className="logo_policji" src={LANDSCAPE_1280} alt="logo" />
+      </div>
+      <div className="home_right">
+        <div className="home_right_title">GEOPORTAL</div>
+        <div className="home_right_subtitle">
+          Zarządzanie jednostkami policji i policjantami przypisanymi do danej
+          jednostki
+        </div>
+        <button className="home_right_button">START</button>
+        <div className="home_top_about">Informacje ogólne</div>
       </div>
     </div>
   );
 }
-
 export default Home;
